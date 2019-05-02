35af976d1ba681acb1d0d0d9839ed42a08280f95
scherrma <scherr.martin@gmail.com>
2019-05-02 14:17:46 -0400

    clear out old sum_to

--- scheduler/util.py
+++ scheduler/util.py
@@ -11,24 +11,6 @@ def sum_to(options, goal, picks, force_take_all=False):
         selected = [goal]
     else:
         while goal >= min(options) and picks > 0:
-            selected += [min(options, key=lambda x: abs(x - goal/picks))] #pick near the average
-            goal -= selected[-1]
-            picks -= 1
-        if force_take_all and max(options) - selected[-1] >= goal:
-            selected[-1] += goal
-    return selected
-
-def alt_sum_to(options, goal, picks, force_take_all=False):
-    """Returns numbers that sum as close to a goal value as possible.
-
-    options -- the numbers to select from
-    goal -- the value to attempt to sum to
-    picks -- the length of the list to be returned"""
-    selected = []
-    if goal < min(options):
-        selected = [goal]
-    else:
-        while goal >= min(options) and picks > 0:
             selected.append(min((x for x in options if x >= goal/picks),
                                 key=lambda x: abs(x - goal/picks), default=options[-1]))
             goal -= selected[-1]
